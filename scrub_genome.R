0b8381e9bbda4d39ad7ee4f568a4fbd4958caff2
Alana Alexander <laninsky@gmail.com>
2016-12-14 16:34:58 -0600

    Update scrub_genome.R

--- scrub_genome.R
+++ scrub_genome.R
@@ -10,6 +10,7 @@ range_to_delete <- matrix(nrow=(length(scaffoldnames)),ncol=2)
 range_to_delete[,1] <- scaffoldnames
 
 for (i in scaffoldnames) {
+  to_delete <- NULL
   subscaff <- contam[(which(contam[,2]==i)),9:10]
 
   if (!(is.matrix(subscaff))) {  
@@ -22,25 +23,43 @@ for (i in scaffoldnames) {
   
   } else {
    
+    subscaff <- cbind(as.numeric(subscaff[,1]),as.numeric(subscaff[,2]))
+    
     for (j in 1:(dim(subscaff)[1])) {
-      if(as.numeric(subscaff[j,1])>as.numeric(subscaff[j,2])) {
+      if(subscaff[j,1]>subscaff[j,2]) {
         tempsubscaff <- subscaff[j,1]
         subscaff[j,1] <- subscaff[j,2]
         subscaff[j,2] <- tempsubscaff
       }
     }
-    
-    subscaff[,1] <- as.numeric(subscaff[,1])
-    subscaff[,2] <- as.numeric(subscaff[,2])
-    subscaff <- subscaff[(order(subscaff[,2])),]
-    subscaff <- subscaff[(order(subscaff[,1])),]
+
+    subscaff <- subscaff[order(subscaff[,2]),]
+    subscaff <- subscaff[order(subscaff[,1]),]
     
     tempscaff <- as.matrix(subscaff[1,],nrow=2)
     
     for (j in 2:(dim(subscaff)[1])) {
-      if(subscaff[j,1]<tempscaff[2,(dim(tempscaff[
-  }
-
-
-
+      if((subscaff[j,1]>=tempscaff[1,(dim(tempscaff)[2])]) && (subscaff[j,1]<tempscaff[2,(dim(tempscaff)[2])]) && (subscaff[j,2]>tempscaff[1,(dim(tempscaff)[2])]) && (subscaff[j,2]<=tempscaff[2,(dim(tempscaff)[2])])) {
+        break
+      } else {
+        if((subscaff[j,1]>=tempscaff[1,(dim(tempscaff)[2])]) && (subscaff[j,1]<=(tempscaff[2,(dim(tempscaff)[2])]+50))) {
+          tempscaff[2,(dim(tempscaff)[2])] <- subscaff[j,2]
+          break
+        }
+        temp <- rbind(subscaff[j,1],subscaff[j,2])
+        tempscaff <- cbind(tempscaff,temp)
+      }
+    }
+  
+   to_delete <- paste(tempscaff[1,1],":",tempscaff[2,1],sep="")
+   
+   if(dim(tempscaff)[2]>1) { 
+      for (j in 2:(dim(tempscaff)[2])) {
+        to_delete <- paste(to_delete,paste(tempscaff[1,j],":",tempscaff[2,j],sep=""),sep=",")
+      } 
+   }
+      
+      }
 range_to_delete[which(scaffoldnames==i),2] <- to_delete
+
+} 
