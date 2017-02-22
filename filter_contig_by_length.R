6e38aec805383a6cb8a642539b27f3f58f516233
Alana Alexander <laninsky@gmail.com>
2017-02-22 10:35:50 -0600

    Update filter_contig_by_length.R

--- filter_contig_by_length.R
+++ filter_contig_by_length.R
@@ -1,4 +1,17 @@
-filter_contig_by_length <- function(
+filter_contig_by_length <- function(file_to_filter,contig_length) {
+
+if(missing(file_to_filter) | missing(contig_length)) {
+  print(paste("This script needs you to define the location and name of your fasta file and the minimum contig length you want to retain in the outfile: length_filtered_genome.fasta"))
+  print(paste("Example of calling filter_contig_by_length:"))
+  cat('filter_contig_by_length("/mnt/anolis/Braker/scrubbed_genome.fasta.masked",1000)\n\n')
+  stop("Please fill in the missing info in your function call")
+}
+    
+#Setting the variable to record sequence (we are going to write out all the sequence on one line for each scaffold, rather than having linebreaks in teh sequence)
+sequencerec <- NULL
+
+con <- file(file_to_filter)
+open(con)
 
 while ( TRUE ) {
     line = readLines(con, n = 1)
@@ -8,36 +21,17 @@ while ( TRUE ) {
     }
     #If there is a ">" in the line, and we've previously recorded sequence (i.e. we won't do this for the first line in the file)
     if (grepl(">",line,fixed=TRUE) && (!(is.null(sequencerec)))) {
-        #If the previous contig needs to be scrubbed for contamination
-        if (gsub(">","",seqname,fixed=TRUE) %in% range_to_delete[,1]) {
-        
-          #Splitting the DNA string into a vector so we can extract the contaminated positions
-          sequencerec <- unlist(strsplit(sequencerec,split=""))
-        
-          #Obtaining the contaminated positions in an array from the range_to_delete matrix  
-          toremove <- unlist(strsplit(range_to_delete[(which(range_to_delete[,1]==gsub(">","",seqname,fixed=TRUE))),2],split=","))
-          toremovemod <- NULL  
-          for (i in toremove) {
-            temp <- as.numeric(unlist(strsplit(i,split=":")))
-            toremovemod <- c(toremovemod,temp[1]:temp[2])
-          }
-        
-        #Removing those positions and collapsing the sequence back into a single string
-          sequencerec <- sequencerec[-toremovemod]
-          sequencerec <- paste(sequencerec,collapse="")
-        }
       #Getting the length of the sequence
       sequencelength <- nchar(sequencerec)
-      
-      #If the sequence is longer than 100, finding the non-gapped length, and writing out the scaffold name, length and non-gapped length to "name_lengths_Ns.txt". Writing out the scaffold name and sequence to "scrubbed_genome.fasta"
-      if(sequencelength>=100) {
+      #If the sequence is longer than our defined length, finding the non-gapped length, and writing out the scaffold name, length and non-gapped length to "name_lengths_Ns.txt". Writing out the scaffold name and sequence to "scrubbed_genome.fasta"
+      if(sequencelength>=contig_length) {
         sequencelengthN <- nchar(gsub("N","",sequencerec,fixed=TRUE))
         lengthdist <- t(as.matrix(c(seqname,sequencelength,sequencelengthN)))
-        write.table(lengthdist,"name_lengths_Ns.txt",append=TRUE,quote=FALSE,row.names=FALSE,col.names=FALSE)
-        write.table(seqname,"scrubbed_genome.fasta",append=TRUE,quote=FALSE,row.names=FALSE,col.names=FALSE)
-        write.table(sequencerec,"scrubbed_genome.fasta",append=TRUE,quote=FALSE,row.names=FALSE,col.names=FALSE)
+        write.table(lengthdist,"name_lengths_Ns_filtered.txt",append=TRUE,quote=FALSE,row.names=FALSE,col.names=FALSE)
+        write.table(seqname,"length_filtered_genome.fasta",append=TRUE,quote=FALSE,row.names=FALSE,col.names=FALSE)
+        write.table(sequencerec,"length_filtered_genome.fasta",append=TRUE,quote=FALSE,row.names=FALSE,col.names=FALSE)
       }
-      #resetting the sequencerec value ang changing the sequencename to the line we just read in
+      #resetting the sequencerec value and changing the sequencename to the line we just read in
       sequencerec <- NULL
       seqname <- line
     } else {
@@ -52,30 +46,15 @@ while ( TRUE ) {
 }        
 
 # The following code writes out the last sequence in the file if it is over 100bp
-if (gsub(">","",seqname,fixed=TRUE) %in% range_to_delete[,1]) {
-  sequencerec <- unlist(strsplit(sequencerec,split=""))
-        
-  toremove <- unlist(strsplit(range_to_delete[(which(range_to_delete[,1]==gsub(">","",seqname,fixed=TRUE))),2],split=","))
-  toremovemod <- NULL  
-  for (i in toremove) {
-    temp <- as.numeric(unlist(strsplit(i,split=":")))
-    toremovemod <- c(toremovemod,temp[1]:temp[2])
-  }
-        
-  sequencerec <- sequencerec[-toremovemod]
-  sequencerec <- paste(sequencerec,collapse="")
-}
-
 sequencelength <- nchar(sequencerec)
-  if(sequencelength>=100) {
+  if(sequencelength>=contig_length) {
     sequencelengthN <- nchar(gsub("N","",sequencerec,fixed=TRUE))
     lengthdist <- t(as.matrix(c(seqname,sequencelength,sequencelengthN)))
-    write.table(lengthdist,"name_lengths_Ns.txt",append=TRUE,quote=FALSE,row.names=FALSE,col.names=FALSE)
-    write.table(seqname,"scrubbed_genome.fasta",append=TRUE,quote=FALSE,row.names=FALSE,col.names=FALSE)
-    write.table(sequencerec,"scrubbed_genome.fasta",append=TRUE,quote=FALSE,row.names=FALSE,col.names=FALSE)
+        write.table(lengthdist,"name_lengths_Ns_filtered.txt",append=TRUE,quote=FALSE,row.names=FALSE,col.names=FALSE)
+        write.table(seqname,"length_filtered_genome.fasta",append=TRUE,quote=FALSE,row.names=FALSE,col.names=FALSE)
+        write.table(sequencerec,"length_filtered_genome.fasta",append=TRUE,quote=FALSE,row.names=FALSE,col.names=FALSE)
   }
-
-write.table(range_to_delete,"ranges_deleted_from_scaffolds.txt",append=TRUE,quote=FALSE,row.names=FALSE,col.names=FALSE)
 
 #Closing the connection to the file
 close(con)
+}
